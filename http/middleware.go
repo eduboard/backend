600bcc8ddf590b0add433c206b704bd9949a680f
cosmonawt <cosmonawt@protonmail.ch>
2018-06-09 18:54:42 +0200

    http: Add CORS middleware

--- http/middleware.go
+++ http/middleware.go
@@ -1,9 +1,9 @@
 package http
 
 import (
+	"fmt"
 	"github.com/eduboard/backend"
 	"net/http"
-	"fmt"
 )
 
 // Chain takes a final http.Handler and a list of Middlewares and builds a call chain such that
@@ -39,6 +39,20 @@ func NewAuthMiddleware(provider eduboard.UserAuthenticationProvider) func(handle
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%s: %s %s\n", r.RemoteAddr, r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
