8dfbc3916293e8118886fb3a9b4e8874dc66b94e
cosmonawt <cosmonawt@protonmail.ch>
2018-06-14 09:13:38 +0200

    Fix CORS Header

--- http/middleware.go
+++ http/middleware.go
@@ -45,7 +45,7 @@ func Logger(next http.Handler) http.Handler {
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
 
